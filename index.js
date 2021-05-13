c405526d8c80c57746c740ce5aa17e53dbe87903
IfTrueReturnTrue <>
2021-05-13 00:14:33 +0000

    stuff

---
+++ index.js
@@ -0,0 +1,45 @@
+console.log("yes")
+
+const Discord = require('discord.js');
+
+const fs = require("fs");
+const jsonfile = require("jsonfile")
+const bot = new Discord.Client();
+const { int } = require('random');
+const keepAlive = require('./server');
+bot.on('message', (message)=> {
+  
+})
+keepAlive()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot.login('ODM0MzY5NDk1NTA1NDM2Njcy.YH_5JQ.Lmy-E-yOHayYVqB2bRPQI7Sdrbo')
